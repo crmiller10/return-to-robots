7bcfb0cf88fe306d3e2b043d03b0683fad29b315
Collin Miller <crmiller10@gmail.com>
2017-08-22 15:54:57 -0400

    Fixed available for hire text

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ mongo.connect('mongodb://localhost:27017/test', function(err, db) {
   let robots = db.collection('robots');
 
   for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].company === null) {
+      (data.users[i].company = "Available for Hire");
+    }
     robots.insert(data.users[i]);
   }
   db.close();
